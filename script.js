e190367d121b7f5bc89ac7d1aaedb91654572bb3
Paweł Wolski <pawelwolski3@gmail.com>
2019-10-29 14:55:47 +0100

    last changes

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-window.addEventListener('keydown', function (e){console.log(e)}
+function playSound(e) {
+  const sound = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+  if (!sound) return;
+  sound.currentTime = 0;
+  sound.play()
 
-)
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+  key.classList.add('playing')
+}
+window.addEventListener('keydown', playSound)
+
+function removeTransition(e) {
+  e.target.classList.remove('playing');
+}
+const keys = document.querySelectorAll('.key');
+
+keys.forEach(key => key.addEventListener('transitionend', removeTransition))
